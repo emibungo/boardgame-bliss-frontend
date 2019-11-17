fbdae497d3f5d5e06429d75a27fafa41fe82ae39
emibungo <emi.bungo@gmail.com>
2019-11-17 18:40:50 -0500

    Added default background color and background image to CSS

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,6 +2,6 @@ const Deact = require("../libs/deact");
 const Container = require("./Container");
 
 function MainContent() {
-  return Deact.create({ class: "main-content" }, "");
+  return Deact.create("main", { class: "main-content" }, "");
 }
 module.exports = MainContent;
